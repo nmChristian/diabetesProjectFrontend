6bac62f76704a95d34a277a8f9e647e6a22ba4a8
Jonas Vendelbo Hansen <Joni1234@hotmail.dk>
2022-03-24 17:28:32 +0100

    Chart working

--- src/views/JonasView.vue
+++ src/views/JonasView.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>
-    <h1>
-      Fem flade fisk
-    </h1>
-    <h2>
-      Nej det er der ikke
-    </h2>
+  <div class="jonasdiv">
+    <h1> Chart is here </h1>
+    <Chart
+        :axisData="axisData"
+        :coupons="coupons"
+        title="Tickets vs. Value"
+        :companies="companies"
+    />
   </div>
 </template>
+
+
+<script setup lang="ts">
+import Chart from "@/components/Chart.vue"
+import { coupons } from "@/assets/coupons.json"
+
+import { computed } from 'vue'
+
+const axisData = computed(() => coupons
+.filter((el : any) => el.promotion_type === 'percent-off')
+.map((el : any) => ({x: el.coupon_id, y: el.value, company: el.webshop_id})))
+
+const companies = computed( ()=> [...new Set(coupons.map((el : any) => el.webshop_id))])
+
+</script>
