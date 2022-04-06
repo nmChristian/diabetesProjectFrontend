80fb21da45dfecf901dc26ca050d4f38092d6c11
Jonas Vendelbo Hansen <joni1234@hotmail.dk>
2022-04-06 23:15:23 +0200

    Added graphs and now a now a method to apply SVG

---
+++ src/services/graphs/graphs.ts
@@ -0,0 +1,66 @@
+import * as d3 from "d3";
+import type {Ref} from "vue";
+
+interface Data {
+    date: Date,
+    value: number
+}
+interface Point {
+    x: number,
+    y: number
+}
+
+export default class Graphs {
+
+    readonly colorScheme : string[] = ["#33658a","#78c0e0","#5da271","#dda448","#92140c"]
+
+    getDataNDaysBack = (data : Data [], days : number) => data.filter(d => (d.date).getTime() > data[data.length - 1].date.getTime() - days * 24 * 60 * 60 * 1000)
+    getTimeOfDayInSeconds = (date : Date) => date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds()
+
+    medianData (data : Data [], daysBack : number, dataPointsPerHour : number) {
+        const splitByTimeOfDayData : d3.Bin<Data, number>[] = d3.bin<Data, number>()
+            .value(d  => this.getTimeOfDayInSeconds(d.date))
+            .thresholds(dataPointsPerHour * 24)
+            (this.getDataNDaysBack(data, daysBack))
+
+        // @ts-ignore
+        const hours = splitByTimeOfDayData.map((a : d3.Bin<Data, number>) => ((a.x0 + a.x1) / 2) / 3600)
+
+        const points = splitByTimeOfDayData.map((a,i) => ({x:hours[i],  y: d3.median(a, d => d.value) ?? 0}) )
+
+        return points
+    }
+
+    iconChart (medianData : Point[], clr : string, {
+        width = 80, // outer width, in pixels
+        height = 60, // outer height, in pixels
+        strokeWidth = 3,    // Background and stroke
+    }) {
+
+        const xScale = d3.scaleLinear( [0, 24], [0, width])
+        const yScale = d3.scaleLinear([0, 350], [height, 0])
+
+        const svg = d3.create("svg")
+        //const svg = d3.select(svgElement)
+            .attr("width", width)
+            .attr("height", height)
+            .attr("style", "max-width: 100%; height: auto; height: intrinsic;")
+
+        svg.attr("style", "border:" + clr + "  solid " + strokeWidth + "px; border-radius: 20px;")
+
+        // DRAW MEDIAN
+        const medianLineGen : d3.Line<Point> = d3.line<Point>()
+            .curve(d3.curveLinear)
+            .x((d) => xScale(d.x))
+            .y((d) => yScale(d.y))
+        svg.append("path")
+            .attr("style", "stroke-width: " + strokeWidth + "; fill: none; stroke: " + clr + ";  opacity: 1;")
+            .attr("d", medianLineGen(medianData))
+
+        return svg.node()
+    }
+
+    applySVG (ref : Ref<SVGElement | null>, svg : SVGSVGElement | null) {
+        if (ref.value != null && svg?.outerHTML != undefined) ref.value.outerHTML = svg.outerHTML
+    }
+}
--- src/views/JonasView.vue
+++ src/views/JonasView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="jonasdiv">
+    <svg ref ="svg"></svg>
     <h1> Chart is here </h1>
     <Tidytree
         title="Fisk Chart"
@@ -14,20 +15,29 @@
       :data="unemployment"
       :att="att"
     />
-
   </div>
 </template>
 
+<script lang="ts">
 
+</script>
 <script setup lang="ts">
 // https://vuejsexamples.com/an-interactive-vue-and-d3-js-app-that-fetches-data-from-a-json-file-and-displays-data-on-a-chart/
+import { ref, Ref, onMounted } from 'vue'
 import { coupons } from "@/assets/chart/examples/coupons.json"
 import { unemployment } from "@/assets/chart/examples/unemployment.json"
+import cgm from "@/assets/demo/cgm.json"
+
 
 import {computed} from 'vue'
 import Histogram from "@/components/charts/Histogram.vue";
 import HistoBetter from "@/components/charts/HistoBetter.vue"
 import Tidytree from "@/components/charts/Tidytree.vue"
+import Graph from "@/services/graphs/graphs"
+
+const graph = new Graph ();
+const cgmMGDL = cgm.map((d) => ({date: new Date(d.date), value: d.cgm * 18}))
+
 
 const att = {
     value: (d : any) => d.rate,
@@ -40,10 +50,16 @@ const att = {
 const axisData = computed(() => coupons
     .filter((el : any) => el.promotion_type === 'percent-off')
     .map((el : any) => ({x: el.coupon_id, y: el.value, company: el.webshop_id})))
-
 const companies = computed( ()=> [...new Set(coupons.map((el : any) => el.webshop_id))])
 
-// https://observablehq.com/@d3/histogram
+// Reference
+const svg : Ref<SVGElement | null> = ref(null)
+onMounted(() => {
+  const medData = graph.medianData(cgmMGDL, 2, 1 )
+  const chart = graph.iconChart(medData, graph.colorScheme[2], {})
+  graph.applySVG(svg, chart)
+
+})
 
 
 </script>
