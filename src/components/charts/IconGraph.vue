b3ba755d1897faddf629524cd7d3402090de89e9
Jonas Vendelbo Hansen <joni1234@hotmail.dk>
2022-04-06 23:15:39 +0200

    Added graphs and now a now a method to apply SVG

---
+++ src/components/charts/IconGraph.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script setup lang="ts">
+import Vue from 'vue-class-component'
+
+export class IconGraph extends Vue {
+  // Initial data can be declared as instance properties
+  message: string = 'Hello!'
+
+  // Component methods can be declared as instance methods
+  onClick (): void {
+    window.alert(this.message)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
