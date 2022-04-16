4ea9bee26ce745539d84f4583477e8dcd45adf80
Christian <s204424@dtu.dk>
2022-04-16 14:50:14 +0200

    Icon data works

--- src/views/DisplayPatientsList.vue
+++ src/views/DisplayPatientsList.vue
@@ -1,25 +1,22 @@
 <template>
   <div>
     <ListViewPatientElement
-        v-for="u in users"
+        v-for="u in this.$data.users"
         :name = 'u.name'
         :cpr = 'u.cpr'
         :age= 'u.age'
         :data = 'u.medData'
         :doctor="false"
         :selected="u.id === 1"
-        :status = 'u.status'>
+        :status = 'u.status'
+        @click="clickedItem(u.id)">
 
     </ListViewPatientElement>
-
-    <icon-graph
-      :median-data="users[1].medData"
-      :status="users[1].status"></icon-graph>
 
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 import {computed} from 'vue'
 import IconGraph from "@/components/charts/IconGraph.vue";
 import cgm from "@/assets/demo/cgm.json";
@@ -31,25 +28,34 @@ import cgm_538 from "@/assets/demo/users/cgm_538.json"  // Lowest and nice 100%
 
 import {DataEdit} from "@/services/graphs/graphs";
 
-const editor = new DataEdit ();
+export default {
+  data(){
+    let editor = new DataEdit ()
+    let cgmMGDL = cgm.map((d) => ({date: new Date(d.date), value: d.cgm * 18}))
+    let cgmMGDL_083 =  editor.convertToMGDL(cgm_083)
+    let cgmMGDL_123 = editor.convertToMGDL(cgm_123)
+    let cgmMGDL_200 = editor.convertToMGDL(cgm_200)
+    let cgmMGDL_538 = editor.convertToMGDL(cgm_538)
+    return {
 
+      medData:  computed(() => editor.medianData(cgmMGDL, 7, 1 )),
+      users: computed(() =>
+          [{id:0, name: "Alexander",  age:2 , cpr:"ddmmyy-xxx1", status: 0 ,medData: editor.medianData(cgmMGDL_083, 7, 1 )},
+            {id:1, name: "Christian", age:2 , cpr:"ddmmyy-xxx2", status: 0, medData: editor.medianData(cgmMGDL_123, 7, 1 )},
+            {id:2, name: "Niels",     age:2 , cpr:"ddmmyy-xxx3", status: 1, medData: editor.medianData(cgmMGDL_200, 7, 1 )},
+            {id:3, name: "Jonas",     age:2 , cpr:"ddmmyy-xxx4", status: -1, medData: editor.medianData(cgmMGDL_538, 7, 1 )}])
 
-// Convert from mmol/L to mg/dL
-const cgmMGDL = cgm.map((d) => ({date: new Date(d.date), value: d.cgm * 18}))
-const cgmMGDL_083 = editor.convertToMGDL(cgm_083)
-const cgmMGDL_123 = editor.convertToMGDL(cgm_123)
-const cgmMGDL_200 = editor.convertToMGDL(cgm_200)
-const cgmMGDL_538 = editor.convertToMGDL(cgm_538)
+    }
 
-const medData = computed(() => editor.medianData(cgmMGDL, 7, 1 ))
-const users = computed(() =>
-    [{id:0, name: "Alexander",  age:2 , cpr:"ddmmyy-xxx1", status: 0 ,medData: editor.medianData(cgmMGDL_083, 7, 1 )},
-      {id:1, name: "Christian", age:2 , cpr:"ddmmyy-xxx2", status: 0, medData: editor.medianData(cgmMGDL_123, 7, 1 )},
-      {id:2, name: "Niels",     age:2 , cpr:"ddmmyy-xxx3", status: 1, medData: editor.medianData(cgmMGDL_200, 7, 1 )},
-      {id:3, name: "Jonas",     age:2 , cpr:"ddmmyy-xxx4", status: -1, medData: editor.medianData(cgmMGDL_538, 7, 1 )}])
-
-
+  },
+  methods:{
+    clickedItem(id: Number){
+      alert("hey" + (id))
+    }
+  }
+}
 </script>
+
 
 <style scoped>
 
