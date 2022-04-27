064e2e79277ff95bce9f813fdc536576e9c4be17
niller3003 <nielstg@live.dk>
2022-04-28 00:01:52 +0200

    Some sing-in impovements

---
+++ src/components/input/AnimatedTextInput.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="form">
+        <input class="input" placeholder=" " />
+        <label class="label">{{ labelText }}</label>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: "animatedTextInput",
+    props: {
+        labelText: String
+    }
+}
+
+</script>
+
+<style scoped>
+.form {
+    position: relative;
+    width: 20rem;
+    font-size: 1.4rem;
+}
+
+input {
+    position: absolute;
+    width: 100%;
+    padding-left: 0.6rem;
+    background: none;
+    border-radius: 5px;
+    border: 1px solid;
+    outline: none;
+    font-size: inherit;
+    font-family: inherit;
+}
+
+label {
+    top: -1px;
+    left: 0.6rem;
+    transition: all .4s;
+    background-color: white;
+    color: gray;
+}
+
+input:hover {
+    border: 2px solid;
+}
+
+input:focus {
+    border: 1px solid blueviolet;
+}
+
+input:focus~.label,
+.input:not(:placeholder-shown).input:not(:focus)~label {
+    top: -1.4rem;
+    font-size: 50%;
+    color: lightgray;
+}
+</style>
--- src/views/authentication/SignInView.vue
+++ src/views/authentication/SignInView.vue
@@ -1,24 +1,40 @@
 <template>
-    <img src="@/assets/logo.svg" width="125" height="125" />
-    <div class="signIn">
-        <label>E-mail:</label>
-        <input v-model="email" name="usernameField">
+    <img src="@/assets/logo.svg" width="250" height="250" />
+    <div>
+        <animated-text-input label-text="E-mail" />
     </div>
     <div>
-        <label>Password:</label>
-        <input v-model="password" name="passwordField">
+        <animated-text-input label-text="Password" />
     </div>
-    <div>
+    <!--<div>
         <link-button href="#" linkText="Forgot Password" name="forgotPasswordLink">Forgot password</link-button>
     </div>
     <div>
         <button name="signInButton">Sign-in</button>
-    </div>
+    </div> 
+     -->
 </template>
 
 <script lang = "ts">
-export default {}
+import animatedTextInput from "../../components/input/AnimatedTextInput.vue"
+
+export default {
+    components: {
+        animatedTextInput
+    }
+}
 </script>
 
-<style>
+<style scoped>
+img {
+    display: block;
+    margin: auto;
+    padding: 2rem;
+}
+
+div {
+    display: block;
+    margin: auto;
+    padding: 0.2em;
+}
 </style>
