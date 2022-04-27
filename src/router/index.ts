d052edc7e018b9986f8293341d7095900a92437c
niller3003 <nielstg@live.dk>
2022-04-28 00:07:26 +0200

    Fix merge error of missing router link

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,11 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/SignIn',
+      name: 'SignIn',
+      component: () => import('../views/authentication/SignInView.vue')
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
