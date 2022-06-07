7ac1d7553bb3182993195e5b73d7c4af458b2040
Christian <s204424@dtu.dk>
2022-06-07 15:08:42 +0200

    Fixed position on list patients. And added the summary page (no content)

---
+++ src/views/Summary.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="outerHolderSummary">
+<h1>Summary information about all patients, accessible to the current user</h1>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "Summary"
+}
+</script>
+
+<style scoped>
+
+.outerHolderSummary{
+  height: 100%;
+  width: 100%;
+  border: red 1px dotted;
+}
+
+</style>
