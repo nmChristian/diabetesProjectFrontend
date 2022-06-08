5630cad98d88928878c34ec0464d60c4f7421adf
Jonas Vendelbo Hansen <Joni1234@hotmail.dk>
2022-06-08 14:30:25 +0200

    Can now change interval by the use of an option ( this is pretty buggy but it works )

--- src/components/graphview/test/CGMGraphsTest.vue
+++ src/components/graphview/test/CGMGraphsTest.vue
@@ -1,19 +1,31 @@
 <template>
   <div>
     <h1>Vejrudsigt graf</h1>
+
+    <div style ="display: grid; place-items: center;">
+      <p>Split by</p>
+      <select v-model="interval" style="font-size: 16px; text-align: center; width: 300px; height: 35px; border-radius: 20px;" >
+        <option :value="d3.timeMonday">Monday</option>
+        <option :value="d3.timeSunday">Sunday</option>
+        <option :value="d3.timeMonth">Month</option>
+        <option :value="d3.timeHour">Hour</option>
+      </select>
+    </div>
+      <br>
+
     <forecast-graph
         :data="dataSplitIntoIntervals.get(lastThreeIntervals[2]) ?? []"
-        :time-interval="timeInterval"
+        :time-interval="interval"
         :graph-layout="graphLayout"
     />
     <forecast-graph
         :data="dataSplitIntoIntervals.get(lastThreeIntervals[1]) ?? []"
-        :time-interval="timeInterval"
+        :time-interval="interval"
         :graph-layout="graphLayout"
     />
     <forecast-graph
         :data="dataSplitIntoIntervals.get(lastThreeIntervals[0]) ?? []"
-        :time-interval="timeInterval"
+        :time-interval="interval"
         :graph-layout="graphLayout"
     />
 
@@ -24,17 +36,19 @@
 import type {DateValue, Point} from "@/services/core/datatypes";
 import {GraphLayout} from "@/services/core/graphtypes";
 import ForecastGraph from "@/components/charts/ForecastGraph.vue";
-import {computed} from "vue";
+import {computed, ref} from "vue";
 import * as d3 from "d3";
+
+
+const interval = ref(d3.timeSunday)
 
 const props = defineProps<{
   data: DateValue[],
   medianDataInHours: Point[]}>
 ()
-const timeInterval = d3.timeMonday
 const lastDateInDataSet = computed( () => props.data.length === 0 ? new Date() : props.data[props.data.length - 1][0])
-const lastThreeIntervals = computed( () => [0,1,2].map<Date>(back => timeInterval.offset(timeInterval(lastDateInDataSet.value), - back)))
-const dataSplitIntoIntervals = computed( () => d3.group(props.data, ([date,]) => timeInterval(date)))
+const lastThreeIntervals = computed( () => [0,1,2].map<Date>(back => interval.value.offset(interval.value(lastDateInDataSet.value), - back)))
+const dataSplitIntoIntervals = computed( () => d3.group(props.data, ([date,]) => interval.value(date)))
 
 const graphLayout = new GraphLayout(1000,100, 0, 40, 20, 40)
 
