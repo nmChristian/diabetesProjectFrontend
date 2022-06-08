61017334f7bea29f19b65ae441ee0e6d0d74f724
Jonas Vendelbo Hansen <joni1234@hotmail.dk>
2022-06-08 12:49:54 +0200

    Vejrudsigt now shows data up to 3 weeks back

--- src/components/graphview/test/CGMGraphsTest.vue
+++ src/components/graphview/test/CGMGraphsTest.vue
@@ -1,21 +1,19 @@
 <template>
   <div>
-    <h2>Get last three mondays </h2>
-    <p>{{ lastThreeMondays }}</p>
-
     <h1>Vejrudsigt graf</h1>
     <forecast-graph
-        :data="data"
+        :data="dataSplitIntoMondays.get(lastThreeMondays[2]) ?? []"
         :graph-layout="graphLayout"
     />
     <forecast-graph
-        :data="data"
+        :data="dataSplitIntoMondays.get(lastThreeMondays[1]) ?? []"
         :graph-layout="graphLayout"
     />
     <forecast-graph
-        :data="data"
+        :data="dataSplitIntoMondays.get(lastThreeMondays[0]) ?? []"
         :graph-layout="graphLayout"
     />
+
   </div>
 </template>
 
@@ -25,6 +23,7 @@ import {GraphLayout} from "@/services/core/graphtypes";
 import ForecastGraph from "@/components/charts/ForecastGraph.vue";
 import {computed} from "vue";
 import {getLastMondays} from "@/services/core/dateMethods";
+import * as d3 from "d3";
 
 const props = defineProps<{
   data: DateValue[],
@@ -33,8 +32,8 @@ const props = defineProps<{
 
 const lastDateInDataSet = computed( () => props.data.length === 0 ? new Date() : props.data[props.data.length - 1][0])
 const lastThreeMondays = computed( () => [0,1,2].map<Date>(back => getLastMondays(lastDateInDataSet.value, back)))
+const dataSplitIntoMondays = computed( () => d3.group(props.data, ([date,]) => d3.timeMonday(date)))
 
-
-const graphLayout = new GraphLayout(1000,100, 30, 40, 40, 40)
+const graphLayout = new GraphLayout(1000,100, 0, 40, 20, 40)
 
 </script>
