1b1929353a047dd4846ad17c96db6aa049a0dfd8
Christian <s204424@win.dtu.dk>
2022-06-13 16:37:40 +0200

    Removed unused stuff

--- src/components/patientElements/DisplayPatientsFullList.vue
+++ src/components/patientElements/DisplayPatientsFullList.vue
@@ -16,42 +16,21 @@
 </template>
 
 <script setup lang="ts">
-import {computed, ref} from 'vue'
+import {ref} from 'vue'
 
-import cgm_083 from "@/assets/demo/users/cgm_083.json" // 95% 5%
-import cgm_123 from "@/assets/demo/users/cgm_123.json" // 76% 21% 3%
-import cgm_200 from "@/assets/demo/users/cgm_200.json" // 2% 80% 15% 4%
-import cgm_538 from "@/assets/demo/users/cgm_538.json" // Lowest and nice 100%
-import {User} from "@/services/user"
 import router from "@/router"
 import type {DateValue, Point} from "@/services/core/datatypes"
 import {bucketToMedian, SPLIT_BY_DAY, toBuckets, toDateValue} from "@/services/core/datatypes"
 import {HealthLevel} from "@/services/core/shared";
 import backend from "@/services/backend";
-import {UserDetails} from "@/services/core/dbtypes";
+import type {UserDetails} from "@/services/core/dbtypes";
 
 const daysBack = 28
 const RESOLUTION = 96
-const userlist = [
-    new User(0, "Alexander", 199),
-    new User(1, "Christian", 21),
-    new User(2, "Niels", 34),
-    new User(3, "Jonas", 21)]
-
-const cgmToData  = (cgmData : {date: string, cgm: number}[]) : DateValue[] =>
-    toDateValue(cgmData, (data => [new Date(data.date), data.cgm * 18]))
 
 const dataToMedian = (data: DateValue[], split: number): Point[] =>
     bucketToMedian(toBuckets(data, split, RESOLUTION))
 
-
-const users = computed(() =>
-    [ {user: userlist[0], cpr:"ddmmyy-xxx1", healthLevel: HealthLevel.Good ,medianDataInHours: dataToMedian(cgmToData(cgm_083), SPLIT_BY_DAY)},
-      {user: userlist[1], cpr:"ddmmyy-xxx2", healthLevel: HealthLevel.Good, medianDataInHours: dataToMedian(cgmToData(cgm_123), SPLIT_BY_DAY)},
-      {user: userlist[2], cpr:"ddmmyy-xxx3", healthLevel: HealthLevel.High, medianDataInHours: dataToMedian(cgmToData(cgm_200), SPLIT_BY_DAY)},
-      {user: userlist[3], cpr:"ddmmyy-xxx4", healthLevel: HealthLevel.Low, medianDataInHours: dataToMedian(cgmToData(cgm_538), SPLIT_BY_DAY)}
-    ]
-)
 
 class UserWithDate {
   user: UserDetails | undefined;
