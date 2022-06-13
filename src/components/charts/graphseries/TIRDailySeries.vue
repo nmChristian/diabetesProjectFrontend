4fc4d951545d0e4d10741189b92726500e7b2622
Jonas Vendelbo Hansen <Joni1234@hotmail.dk>
2022-06-13 21:52:10 +0200

    Changed to sd to sd/avg

--- src/components/charts/graphseries/TIRDailySeries.vue
+++ src/components/charts/graphseries/TIRDailySeries.vue
@@ -21,7 +21,7 @@
             :occurrences="occurrences[i]"
         />
         <p :style="{borderBottom: '6px solid', borderColor: getCGMColor(averages[i]) }">{{ averages[i].toFixed(2) }}</p>
-        <p :style="{marginTop: '10px' }">{{ deviations[i].toFixed(2) }}</p>
+        <p :style="{marginTop: '10px' }">{{ (deviations[i] / averages[i]).toFixed(2) }}</p>
       </div>
     </div>
   </div>
