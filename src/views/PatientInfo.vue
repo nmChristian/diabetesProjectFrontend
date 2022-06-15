0f56d655dc50f95e70e024583c2680b3bede05c7
Christian <s204424@win.dtu.dk>
2022-06-15 23:03:59 +0200

    More epanded element stuff

--- src/views/PatientInfo.vue
+++ src/views/PatientInfo.vue
@@ -312,6 +312,7 @@ async function loadData() {
   padding: 10px;
   width: 100%;
   border: solid 1px #555;
+  min-width: 1300px;;
   max-width: calc(100% - 320px);
   background-color: #fcfcfc;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.6);
