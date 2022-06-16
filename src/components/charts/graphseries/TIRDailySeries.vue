2515c2bf6c6d053c8acdb19805e3040e0e7459d0
Jonas Vendelbo Hansen <Joni1234@hotmail.dk>
2022-06-16 17:06:53 +0200

    Removed average and CV from daily tir series

--- src/components/charts/graphseries/TIRDailySeries.vue
+++ src/components/charts/graphseries/TIRDailySeries.vue
@@ -20,8 +20,8 @@
             :graph-layout="graphLayout"
             :occurrences="occurrences[i]"
         />
-        <p :style="{borderBottom: '6px solid', borderColor: getCGMColor(averages[i]) }">{{ averages[i].toFixed(2) }}</p>
-        <p :style="{marginTop: '10px'}">{{ (deviations[i] * 100 / averages[i]).toFixed(0)  }} %</p>
+        <p v-if="showAdvanced" :style="{borderBottom: '6px solid', borderColor: getCGMColor(averages[i]) }">{{ averages[i].toFixed(2) }}</p>
+        <p v-if="showAdvanced"  :style="{marginTop: '10px'}">{{ (deviations[i] * 100 / averages[i]).toFixed(0)  }} %</p>
       </div>
     </div>
   </div>
