15c007961fc9ab649c1e22daf4b3fab79fa30d02
Christian <s204424@dtu.dk>
2022-06-19 22:47:22 +0200

    Edeting medecin also works, but could look better

--- src/components/DiagnoseAndMedicine.vue
+++ src/components/DiagnoseAndMedicine.vue
@@ -12,27 +12,40 @@
     <template v-if="data && data.length !==0" v-for="(diag,mdecinIndex) in data">
 
       <div class="medicine-line" @mouseover="howeringElement(mdecinIndex,0)" >
-        <p class="diagnoseAndMedicinItems">{{ diag.name }}</p>
+        <p v-if="!(currentlyEdeting.x=== mdecinIndex && currentlyEdeting.y === 0 && isDoctor && showAdvanced)"  class="diagnoseAndMedicinItems">{{ diag.name }}</p>
+        <input v-else v-model="edetingValue" type="text" class="inputFieldForMedAndDia">
 
         <div class="editButton"
                 v-if="currentHowever.x=== mdecinIndex && currentHowever.y === 0 && isDoctor && showAdvanced">
-          <button>Edit</button>
-          <button @click="deleteDiagnose(mdecinIndex)">Delete {{mdecinIndex}}</button>
+          <button v-if="currentlyEdeting.x !== mdecinIndex" @click="startEdeting(mdecinIndex,0)">Edit</button>
+          <button
+              @click="submitEdit(mdecinIndex,0)"
+              v-else-if="currentlyEdeting.x=== mdecinIndex">Save</button>
+          <button @click="deleteDiagnose(mdecinIndex)">Delete</button>
         </div>
 
       </div>
 
       <div @mouseover="howeringElement(mdecinIndex,1)" class="medicine-line">
-        <template v-for="(item,index) in diag.medicine">
+        <template
+            v-if="!(currentlyEdeting.x=== mdecinIndex && currentlyEdeting.y === 1 && isDoctor && showAdvanced)"
+            v-for="(item,index) in diag.medicine">
           <div>
             <p>{{item}}</p>
           </div>
           <p v-if="index !== diag.medicine.length-1" style="padding-right:5px;">, </p>
         </template>
 
+        <input v-else v-model="edetingValue" type="text" class="inputFieldForMedAndDia">
+
+
         <button class="editButton"
-                @click="alert('hej')"
-                v-if="currentHowever.x=== mdecinIndex && currentHowever.y === 1 && isDoctor && showAdvanced">edit</button>
+                @click="startEdeting(mdecinIndex,1)"
+                v-if="!(currentlyEdeting.x=== mdecinIndex) && currentHowever.x=== mdecinIndex && currentHowever.y === 1 && isDoctor && showAdvanced">edit</button>
+
+        <button
+            @click="submitEdit(mdecinIndex,1)"
+            v-else-if="currentlyEdeting.x=== mdecinIndex && currentlyEdeting.y=== 1">Save</button>
 
       </div>
 
@@ -84,6 +97,9 @@ const addingNew = ref(false)
 const diagnoseInputText = ref("")
 const medicineInputText =ref("")
 
+const currentlyEdeting = ref({x:-1,y:-1})
+const edetingValue = ref("")
+
 function oneAddNewDiag(){
   addingNew.value = true
   diagnoseInputText.value = ""
@@ -94,6 +110,35 @@ function deleteDiagnose(index: number){
   backend.deleteDiagnosis(props.data[index]._id.$oid).then((response) => {
     emit("updateDiagnose")
   })
+}
+
+function startEdeting(index : number, textType : textTypes){
+  if(textType === textTypes.diagnose){
+    edetingValue.value = props.data[index].name
+  }else if(textType === textTypes.medicine){
+    edetingValue.value = listToString(props.data[index].medicine)
+  }
+  currentlyEdeting.value = {x: index, y: textType}
+}
+
+function submitEdit(index: number, textType : textTypes) {
+  if(textType === textTypes.diagnose && edetingValue.value ===""){
+    return
+  }
+  let diagnose = ""
+  let medecin = []
+  if(textType == textTypes.diagnose){
+    medecin = props.data[index].medicine
+    diagnose = edetingValue.value
+  }else {
+    medecin = medecinToList(edetingValue.value)
+    diagnose = props.data[index].name
+  }
+  backend.editDiagnosis(props.data[index]._id.$oid,diagnose,medecin).then(()=>{
+    emit("updateDiagnose")
+    edetingValue.value = ""
+    currentlyEdeting.value ={x:-1,y:-1}
+  })
 
 }
 
@@ -103,6 +148,18 @@ function medecinToList(medecinAsString : string) : string[]{
     re[i] = re[i].trim()
   }
   return re
+}
+
+function listToString(inListe: string | any[] | undefined) {
+  let re = ""
+  if (inListe === undefined) {return ""}
+  for (let i = 0; i < inListe.length; i++) {
+    re += String(inListe[i])
+    if (i < inListe.length - 1) {
+      re += ", "
+    }
+  }
+  return re;
 }
 
 function onSavePressed(){
